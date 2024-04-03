bf0a85471a5ebc5ae372213de9d28a4f48dc83e0
ichiro16go <ichi1.9adgjm@gmail.com>
2024-04-03 21:01:55 +0900

    completed!!

--- lib/stopwatch_controller.dart
+++ lib/stopwatch_controller.dart
@@ -4,79 +4,28 @@ import 'dart:io';
 import 'dart:async';
 
 class Stopwatch {
-  DateTime startTime = DateTime(0, 0, 0, 0, 0);
-  int totalTime = 0;
-  bool running = false;
-  String _displayedTime = "00:00.00";
+  // Timer? timer;
+  // void start(int timeInt, String time) {
+  //   timer = Timer.periodic(const Duration(milliseconds: 10), (timer) {
+  //     timeInt++;
+  //     time = displaytime(timeInt);
+  //     print(time);
+  //   });
+  // }
 
-  void printTime(int totalTime) {
-    final minutes = (totalTime ~/ 60000) % 60;
-    final seconds = (totalTime ~/ 1000) % 60;
-    final millisecondsRemaining = totalTime % 1000;
-    String minutesStr = minutes.toString().padLeft(2, '0');
-    String secondsStr = seconds.toString().padLeft(2, '0');
-    String millsecondsStr =
-        (millisecondsRemaining ~/ 10).toString().padLeft(2, '0');
-    print("Time is $minutesStr:$secondsStr.$millsecondsStr");
-    _displayedTime = "$minutesStr:$secondsStr:$millsecondsStr";
+  // void stop() {
+  //   timer?.cancel();
+  // }
+
+  // void reset(int timerInt) {
+  //   timerInt = 0;
+  //   print('reset!!');
+  // }
+
+  String displaytime(int timerInt) {
+    int minutes = timerInt ~/ (6000);
+    int seconds = (timerInt ~/ 100) % 60;
+    int centsecond = timerInt % 100;
+    return '$minutes:$seconds.$centsecond';
   }
-
-  void changeRunningState() {
-    if (running == true) {
-      running = false;
-      print("Stoping");
-    } else {
-      running = true;
-      print("Running");
-    }
-  }
-
-  void start() {
-    startTime = DateTime.now();
-    changeRunningState();
-    print("starttime is $startTime");
-  }
-
-  void startTimeController() {
-    if (running == false) {
-      start();
-    }
-  }
-
-  void stop() {
-    final nowTime = DateTime.now();
-    final mill = nowTime.difference(startTime).inMicroseconds;
-    int temp = totalTime + mill;
-    totalTime = temp;
-    changeRunningState();
-    printTime(totalTime);
-  }
-
-  void StopController() {
-    if (running == true) {
-      stop();
-    }
-  }
-
-  void reset() {
-    totalTime = 0;
-  }
-
-  void resetController() {
-    if (running == true) {
-      stop();
-    } else {}
-    reset();
-  }
-
-  void updateTime() {
-    if (running == true) {
-      final nowTime = DateTime.now();
-      final mill = nowTime.difference(startTime).inMicroseconds;
-      int temp = totalTime + mill;
-      totalTime = temp;
-    }
-  }
-
-  String get displayedTime => _displayedTime;
 }
